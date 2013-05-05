c4e3e3af7d47d9537ffd0966e2199de11e81cd2f
Christian Trimble <christian.trimble@meltmedia.com>
2013-05-05 08:44:08 -0700

    Fixed repo path.

--- comb.rb
+++ comb.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Comb < Formula
   homepage 'https://github.com/ctrimble/combinatorics'
   version '0.3.0-SNAPSHOT'
-  head 'git://github.com/ctrimble/combintorics.git', :branch => 'master', :using => :git
+  head 'git://github.com/ctrimble/combinatorics.git', :branch => 'master', :using => :git
 
   def install
     system "mvn clean install -Dmaven.test.skip=true"
